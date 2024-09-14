b96e78f335a8b3b6501779628a729ba4124cac21
Dmytro Bihniak <bignyak@bigmir.net>
2024-09-14 19:37:36 +0200

    Align dots

--- app/services/impact_report_generator.rb
+++ app/services/impact_report_generator.rb
@@ -3,10 +3,10 @@
 class ImpactReportGenerator
   def generate
     players = Player.joins(:playthroughs)
-          .select("players.*, MIN(playthroughs.score) as first_score, MAX(playthroughs.score) as best_score")
-          .group("players.id")
-          .map { |player| { player: player, impact: player.best_score - player.first_score } }
-          .sort_by { |player| -player[:impact] }
+                    .select("players.*, MIN(playthroughs.score) as first_score, MAX(playthroughs.score) as best_score")
+                    .group("players.id")
+                    .map { |player| { player: player, impact: player.best_score - player.first_score } }
+                    .sort_by { |player| -player[:impact] }
 
     players.map do |player|
       ImpactPlayer.new(player[:player].id, player[:player].name, player[:impact])
